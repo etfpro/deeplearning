bf93e912da33dd51f57b325d4d3bd6efa7b60206
Bohyeon Ma <etfpro@gmail.com>
2017-12-10 00:23:54 +0900

    1
    1

--- DeepLearningFromScratch/optimizer.py
+++ DeepLearningFromScratch/optimizer.py
@@ -14,9 +14,10 @@ class SGD:
             params[key] -= self.lr * grads[key]
 
 
-# Momentum : 기울기 방향 쪽으로 더
+# Momentum
 # Gradient Descent를 통해 이동하는 과정에 일종의 관성/탄력을 주는 방식
 # 현재의 기울기를 통해 이동하는 방향과는 별개로, 과거에 이동했던 방식을 기억하면서 그 방향으로 일정 정도를 추가적으로 이동하는 방식
+# 자주 이동하는 방향에 관성이 걸리게 되고, 진동을 하더라도 중앙으로 가는 방향에 힘을 얻기 때문에 SGD에 비해 상대적으로 빠르게 이동할 수 있다.
 class Momentum:
     def __init__(self, lr=00.1, momentum=0.9):
         self.lr = lr
@@ -38,30 +39,6 @@ class Momentum:
         for key in params.keys():
             self.v[key] = self.momentum * self.v[key] + self.lr * grads[key]
             params[key] -= self.v[key]
-
-
-class Nesterov:
-    """Nesterov's Accelerated Gradient (http://arxiv.org/abs/1212.0901)"""
-
-    # NAG는 모멘텀에서 한 단계 발전한 방법이다. (http://newsight.tistory.com/224)
-
-    def __init__(self, lr=0.01, momentum=0.9):
-        self.lr = lr
-        self.momentum = momentum
-        self.v = None
-
-    def update(self, params, grads):
-        if self.v is None:
-            self.v = {}
-            for key, val in params.items():
-                self.v[key] = np.zeros_like(val)
-
-        for key in params.keys():
-            self.v[key] *= self.momentum
-            self.v[key] -= self.lr * grads[key]
-            params[key] += self.momentum * self.momentum * self.v[key]
-            params[key] -= (1 + self.momentum) * self.lr * grads[key]
-
 
 
 
@@ -94,7 +71,7 @@ class AdaGrad:
 
 # RMSprop - AdaGrad 개선 버전, 학습률 감소
 # AdaGrad는 과거의 기울기를 제곱해서 계속 더해가기 때문에, 학습을 진행할 수록 갱신 강도가 약해짐
-# 실제로 무한히 계속 학습한다면 어느 순간 갱신량이 0이되어 전혀 갱힌되지 않은 문제가 있음
+# 실제로 무한히 계속 학습한다면 어느 순간 갱신량이 0이되어 전혀 갱신되지 않은 문제가 있음
 # 따라서 과거의 모든 기울기를 균일하게 더해가는 것이 아니라, 먼 과거의 기울기는 서서히 잊고 새로운 기울기 정보를 크게 반영 - 지수이동평균
 # 즉, 과거 기울기의 반영 규모를 기하급수적으로 감소시킴
 # 강화학습에서 많이 사용
@@ -112,9 +89,7 @@ class RMSProp:
 
         for key in params.keys():
             # 이전 기울기를 감소시킨다.
-            self.h[key] *= self.decay_rate
-
-            self.h[key] += (1 - self.decay_rate) * grads[key] ** 2
+            self.h[key] = self.decay_rate * self.h[key] + (1 - self.decay_rate) * grads[key] ** 2
             params[key] -= self.lr * grads[key] / (np.sqrt(self.h[key]) + 1e-7)
 
 
@@ -128,8 +103,8 @@ class Adam:
         self.beta1 = beta1 # 1차 momentum용 계수
         self.beta2 = beta2 # 2차 momentum용 계수
         self.iter = 0
-        self.m = None
-        self.v = None
+        self.m = None # RMSProp의
+        self.v = None # Momentum의 관성
 
     def update(self, params, grads):
         if self.m is None:
@@ -157,6 +132,8 @@ class Adam:
 
 
 if __name__ == '__main__':
+    import matplotlib.pyplot as plt
+    from collections import OrderedDict
 
     def f(x, y):
         return x**2 / 20.0 + y**2
@@ -166,8 +143,63 @@ if __name__ == '__main__':
         return x / 10.0, 2.0 * y
 
 
-    # f(x, y) 시작 위치
-    init_pos = (-7.0, 2.0)
+    # optimizer들
+    optimizers = OrderedDict()
+    optimizers["SGD"] = SGD(lr=0.95)
+    optimizers["Momentum"] = Momentum(lr=0.1)
+    optimizers["AdaGrad"] = AdaGrad(lr=1.5)
+    optimizers["Adam"] = Adam(lr=0.3)
 
-    # 가중치 매개변수
+
+    idx = 1
+
+    # 가중치 매개변수 x, y
     params = {}
+
+    # 기울기
+    grads = {}
+    grads['x'], grads['y'] = 0, 0
+
+    # 각 optimizer 별로 기울기 갱신
+    for key in optimizers:
+        optimizer = optimizers[key]
+        x_history = []
+        y_history = []
+        params['x'], params['y'] = -7.0, 2.0
+
+        # 매개변수 갱신(경사하강)
+        for i in range(30):
+            x_history.append(params['x'])
+            y_history.append(params['y'])
+
+            # 기울기 계산(미분) 후, 가중치 갱신
+            grads['x'], grads['y'] = df(params['x'], params['y'])
+            optimizer.update(params, grads)
+
+
+        # 경사 하강 궤적 그래프 그리기
+        x = np.arange(-10, 10, 0.01)
+        y = np.arange(-5, 5, 0.01)
+
+        X, Y = np.meshgrid(x, y)
+        Z = f(X, Y)
+
+        # 외곽선 단순화
+        mask = Z > 7
+        Z[mask] = 0
+
+        # 그래프 그리기
+        plt.subplot(2, 2, idx)
+        idx += 1
+        plt.plot(x_history, y_history, 'o-', color="red")
+        plt.contour(X, Y, Z)
+        plt.ylim(-10, 10)
+        plt.xlim(-10, 10)
+        plt.plot(0, 0, '+')
+        # colorbar()
+        # spring()
+        plt.title(key)
+        plt.xlabel("x")
+        plt.ylabel("y")
+
+    plt.show()
