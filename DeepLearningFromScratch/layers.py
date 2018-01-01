fcc2cb68a9b6756b6e739af15e9c7012f09fe788
Bohyun Ma <etfpro@gmail.com>
2018-01-01 21:26:03 +0900

    1
    1

--- DeepLearningFromScratch/layers.py
+++ DeepLearningFromScratch/layers.py
@@ -2,7 +2,7 @@
 # 역전파 시 해당 함수를 국소적 미분한 값을 곱한다.
 
 import numpy as np
-import functions as func
+
 
 # Affine 변환 계층 (X·W + b)
 class Affine:
@@ -13,19 +13,22 @@ class Affine:
         # 순전파 입력을 역전파(미분) 시 사용을 위해 저장
         self.x = None
 
-        # 가중치에 대한 미분값
+        # 가중치에 대한 미분값 (이전 계층으로 전달하지 않고 내부에서만 유지)
         self.dW = None
 
-        # 편차에 대한 미분값
+        # 편차에 대한 미분값 (이전 계층으로 전달하지 않고 내부에서만 유지)
         self.db = None
 
+        # 입력값의 형상(텐서 대응)
         self.original_x_shape = None
 
 
     def forward(self, x):
-        # 텐서 대응
         self.original_x_shape = x.shape
-        x = x.reshape(x.shape[0], -1) # 행: 데이터의 수, 열: 1개의 입력값을 1행으로 풀어놓은 상태
+
+        # 입력을 행렬(2차원 배열)로 변환
+        # 행: 데이터의 수, 열: 1개의 입력값을 1행으로 풀어놓은 상태
+        x = x.reshape(x.shape[0], -1)
         self.x = x
 
         out = np.dot(self.x, self.W) + self.b
@@ -33,14 +36,15 @@ class Affine:
 
 
     # Affine 함수 미분
-    # dout은 역전파 시킬 값(이전 계층(ReLU등 활성화 함수 계층)의 미분 값)
+    # 입력 - dout 다음 계층(ReLU등 활성화 함수 계층)의 미분 값
+    # 출력 - 이전 계충으로 전파할 값
     def backward(self, dout):
-        dx = np.dot(dout, self.W.T)
-
         self.dW = np.dot(self.x.T, dout)
         self.db = np.sum(dout, axis=0)
 
-        dx = dx.reshape(*self.original_x_shape)  # 입력 데이터 모양 변경(텐서 대응)
+        dx = np.dot(dout, self.W.T)
+        # 행렬(2차원 배열) 형태의 입력 데이터를 원래의 형상으로 변환(텐서 대응)
+        dx = dx.reshape(*self.original_x_shape)
         return dx
 
 
@@ -64,7 +68,8 @@ class Relu:
 
     # ReLU 함수 미분
     # 0보다 작거나 같은 경우 0, 0보다 큰 경우 1
-    # dout은 이전 계층의 미분 값
+    # 입력 - dout은 다음 계층(Affine 계층)의 미분 값
+    # 출력 - 이전 계충으로 전파할 값
     def backward(self, dout):
         dout[self.mask] = 0 # 0 이하인 것들에 대해 0으로 변경
         dx = dout
@@ -88,7 +93,8 @@ class Sigmoid:
 
     # Sigmoid 함수 미분
     # y*(1-y)
-    # dout은 이전 계층의 미분 값
+    # 입력 - dout은 다음 계층의 미분 값
+    # 출력 - 이전 계충으로 전파할 값
     def backward(self, dout):
         dx = dout * (1.0 - self.out) * self.out
         return dx;
