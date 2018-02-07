e86fbc225d8b7abfc037db107bac20b8e4ed5e8c
etfpro <etfpro@gmail.com>
2018-02-07 14:34:39 +0900

    1
    1

---
+++ IntroductionToMachineLearningWithPython/chapter2/3_2.py
@@ -0,0 +1,12 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+import mglearn
+from sklearn.datasets import *
+from sklearn.model_selection import train_test_split
+from sklearn.neighbors import KNeighborsClassifier
+
+
+mglearn.plots.plot_knn_classification(4)
+
+plt.show()
